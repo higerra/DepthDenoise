652b710424f36dd4aa9185462af4421084ab3852
Hang Yan <higerra@126.com>
2014-05-17 18:21:02 +0800

    Initial Commit

---
+++ depthDenoise/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  depthDenoise
+//
+//  Created by 剡航 on 14-5-17.
+//  Copyright (c) 2014年 Yanhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
